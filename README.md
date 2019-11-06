0a284020689d6206a95af0d6dea1cb0831f945c3
Klaus Hörmann-Engl <hoermann.klaus@gmail.com>
2019-11-06 22:36:40 +0100

    [TASK] Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# webzeug.net
+
+This repository holds the content of the website www.webzeug.net. It mainly acts as a proxy point to step into the various smaller websites, which reside under various subdomains. Like "rechnen.webzeug.net" or "klaus.webzeug.net".
---
+++ index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Webzeug.net - Die Website von Klaus Hörmann-Engl</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=yes">
+    <link rel="stylesheet" href="assets/css/style.css" type="text/css">
+    <link rel="stylesheet" href="vendor/twbs/bootstrap/dist/css/bootstrap.css" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Bowlby+One+SC&display=swap" rel="stylesheet">
+    <script src="vendor/components/jquery/jquery.js"></script>
+    <script src="vendor/twbs/bootstrap/dist/js/bootstrap.js"></script>
+    <script src="external/fontawesome5/all.js"></script>
+  </head>
+  <body>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <div class="jumbotron text-center mt-5">
+                    <h1>webzeug.net</h1>
+                    <p class="lead">Willkommen auf meiner Website <strong>webzeug.net</strong>. <br> Mein Name ist Klaus Hörmann-Engl. Ich bin Webentwickler und das ist mein Web-Zeug.</p>
+                    <hr class="my-4">
+                    <p>Hier findet ihr die verschiedensten Projekte an denen ich so bastle. Würde mich freuen wenn ihr euch davon etwas anseht.</p>
+                </div>
+            </div>
+        </div>
+        <div class="row mb-5">
+            <div class="col">
+                <div class="card-deck">
+                    <!-- Klaus -->
+                    <div class="card">
+                        <a href="https://klaus.webzeug.net" target="_blank" title="Mehr über mich erfahren">
+                            <img src="assets/images/klaus.png" class="card-img-top" alt="Bild von mir (Klaus)" />
+                        </a>
+                        <div class="card-body">
+                            <h5 class="card-title text-primary">Über mich</h5>
+                            <p class="card-text">Hier könnt ihr etwas mehr über mich erfahren. Was mich interessiert und was ich so mache.</p>
+                        </div>
+                        <div class="card-footer">
+                            <small class="text-muted">
+                                <a href="https://klaus.webzeug.net" target="_blank">https://klaus.webzeug.net</a>
+                            </small>
+                        </div>
+                    </div>
+                    <!-- Rechnen -->
+                    <div class="card">
+                        <a href="https://rechnen.webzeug.net" target="_blank" title="Mathe Arbeitsblätter erstellen">
+                            <img src="assets/images/rechnen.png" class="card-img-top" alt="Screenshot von Math Arbeitsblatt" />
+                        </a>
+                        <div class="card-body">
+                            <h5 class="card-title text-primary">Mathe Arbeitsblätter</h5>
+                            <p class="card-text">Ein Generator für Mathe Arbeitsblätter. Ihr könnt hier Arbeitsblätter mit Grundrechnungsarten ausdrucken. Super wenn Ihr Volkschulkinder habt.</p>
+                        </div>
+                        <div class="card-footer">
+                            <small class="text-muted">
+                                <a href="https://rechnen.webzeug.net" target="_blank">https://rechnen.webzeug.net</a>
+                            </small>
+                        </div>
+                    </div>
+                    <!-- Blog -->
+                    <div class="card">
+                        COMING SOON
+                    </div>
+                    <!-- MyDailyLog -->
+                    <div class="card">
+                        COMING SOON
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  </body>
+</html>
