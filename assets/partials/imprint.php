95cc3fbfe011c3ea834d2f2a3c1454f95ac059c2
Klaus Hörmann-Engl <hoermann.klaus@gmail.com>
2019-11-08 11:26:34 +0100

    [TASK] Embed footer

---
+++ assets/partials/imprint.php
@@ -0,0 +1,12 @@
+<div class="row">
+    <div class="col">
+        <small class="text-muted">
+            &copy; <?php echo strftime('%G'); ?> Klaus Hörmann-Engl, <a href="mailto:hoermann.klaus@gmail.com">hoermann.klaus@gmail.com</a>
+        </small>
+    </div>
+    <div class="col">
+        <small class="text-muted float-right">
+            <?php echo $version . ', ' . strtoupper($language); ?>
+        </small>
+    </div>
+</div>
